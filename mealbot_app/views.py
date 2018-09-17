d4912db4de8de28d0fe994403cd495de5f061811
aherzfeld <herzfeld.andrew@gmail.com>
2018-09-17 16:46:30 +1000

    adapted all pages to jinja templates, created routes

--- mealbot_app/views.py
+++ mealbot_app/views.py
@@ -4,5 +4,19 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    #return "hello, world"
     return render_template("index.html")
+
+
+@app.route("/addmeal")
+def addmeal():
+    return render_template("addmeal.html")
+
+
+@app.route("/meals")
+def meals():
+    return render_template("meals.html")
+
+
+@app.route("/mealplanner")
+def mealplanner():
+    return render_template("mealplanner.html")
