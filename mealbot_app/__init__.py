009527d335df450fd60f5b5b3659294c87fdb9bc
aherzfeld <herzfeld.andrew@gmail.com>
2018-09-18 11:49:31 +1000

    added config.from_pyfile to override config with instance/config.py

--- mealbot_app/__init__.py
+++ mealbot_app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
+# this will load secret keys from instance/config.py
+app.config.from_pyfile('config.py')
 
 import mealbot_app.views
