e459f74a361dd3a7ef5c5c5afbabaea9f1d0ca49
aherzfeld <herzfeld.andrew@gmail.com>
2018-09-19 16:58:01 +1000

    linked sqlalchemy to postgres

--- mealbot_app/__init__.py
+++ mealbot_app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__, instance_relative_config=True)
@@ -7,6 +7,6 @@ app.config.from_object('config')
 # this will load secret keys from instance/config.py
 app.config.from_pyfile('config.py')
 # instantiate our db with SQLAlchemy
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 import mealbot_app.views
