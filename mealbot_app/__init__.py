d15efc0a100bd633e2c30acb95e0d27436e76177
aherzfeld <herzfeld.andrew@gmail.com>
2018-09-24 18:22:46 +1000

    completed user registration

--- mealbot_app/__init__.py
+++ mealbot_app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__, instance_relative_config=True)
@@ -10,6 +11,9 @@ app.config.from_pyfile('config.py')
 # instantiate our db with SQLAlchemy
 db = SQLAlchemy(app)
 # instantiate our migration engine - part 4 flask mega tutorial
-migrate = Migrate(app, db) 
+migrate = Migrate(app, db)
+login = LoginManager(app)
+# inform flask-login which function handles the logins to enable require login
+login.login_view = 'login'
 
 from mealbot_app import views, models
--- mealbot_app/views.py
+++ mealbot_app/views.py
@@ -1,6 +1,9 @@
-from flask import render_template, flash, redirect, url_for
-from mealbot_app import app
-from mealbot_app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from mealbot_app import app, db
+from mealbot_app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from mealbot_app.models import User
 # import our spoonacular API wrapper package
 import spoonacular as sp
 # Create our spoonacular API class
@@ -14,29 +17,69 @@ def index():
 
 
 @app.route('/addmeal')
+# this will redirect to /login along with query string URL arg so the user will be redirected to the page they wanted prior to login
+@login_required
 def addmeal():
     return render_template('addmeal.html')
 
 
 @app.route('/meals')
+@login_required
 def meals():
     return render_template('meals.html')
 
 
 @app.route('/mealplanner')
+@login_required
 def mealplanner():
     return render_template('mealplanner.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # check if user is already logged in
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()  # instantiates form object
     if form.validate_on_submit():
-        # this is for testing before finishing login system
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are registered!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+
+
+
+
+
 
 
 
