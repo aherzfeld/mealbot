10b4d0a9d1a72c6d706cbcfed53e34faa9351536
aherzfeld <herzfeld.andrew@gmail.com>
2018-10-15 13:02:17 +1000

    heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: flask db upgrade; gunicorn mealbot:mealbot_app
--- mealbot_app/__init__.py
+++ mealbot_app/__init__.py
@@ -22,7 +22,7 @@ bootstrap = Bootstrap(app)
 
 from mealbot_app import routes, models, errors
 
-if not app.debug:
+if not app.debug and not app.testing:
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -38,19 +38,21 @@ if not app.debug:
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/mealbot.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/mealbot.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
 
     # logging categories: DEBUG, INFO, WARNING, ERROR, CRITICAL
     app.logger.setLevel(logging.INFO)
     app.logger.info('MealBot startup')
-
-
-
-
